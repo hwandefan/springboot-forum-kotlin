1db62510d088e57b6a4619208fcc2f080db35bb5
hwandefan <hwandefan@gmail.com>
2023-11-19 14:55:36 +0100

    Added Auth controller for the future auth logic

---
+++ src/main/kotlin/com/hwandefan/forum/api/auth/AuthenticationController.kt
@@ -0,0 +1,10 @@
+package com.hwandefan.forum.api.auth
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/auth")
+class AuthenticationController {
+    
+}
